ceeea814b4d1f36eca721ef2a0b2ea16259c4feb
Magua William <williammagua@gmail.com>
2024-01-27 10:00:07 +0300

    Initialised a JS file

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("Hello, William");
+console.log("Welcome Home");
+console.log("How was Your Day Sir?");
+
+var variableName = "Welcome Home Sir";
+
+console.log(variableName);
+
+let day = "Monday";
+console.log(day);
+
+day = "Friday";
+console.log(day);
+
+const firstName = "William";
+console.log(firstName);
+// firstName = Mwangi;
+
