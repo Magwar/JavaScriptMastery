1a3644995acfece7ed06caa523c89852f5fd81f4
Magwargit config --global user.email williammagua@gmail.com <111179088+Magwar@users.noreply.github.com>
2024-01-27 22:05:34 +0300

    Covered Operators and Data types

--- script.js
+++ script.js
@@ -15,4 +15,46 @@ console.log(day);
 const firstName = "William";
 console.log(firstName);
 // firstName = Mwangi;
+// Objects
+const place = "Munchkin";
+console.log(place);
 
+const person = {
+    name: "John",
+    age: 30,
+    country: "Kenya"
+    };
+console.log(person);
+// Dot Notation
+console.log(person.name);
+console.log(person.country);
+console.log(typeof  person);
+console.log(typeof person.name);
+
+const arr = [1,2,3,4,5,6,7]
+console.log(arr);
+console.log(typeof arr);
+
+const date = new Date();
+console.log(date);
+console.log(typeof date);
+
+// Comparison Operators
+const num1 = 4;
+const num2 = 9;
+// Greater than
+console.log(num1 > num2);
+// Greater Than or Equal to
+console.log(num1 >= num2);
+// Less Than
+console.log(num1 < num2);
+//Less Than Or Equal To
+console.log(num1 <= num2);
+//Equal To
+console.log(num1 == num2);
+//Not equal To
+console.log(num1 != num2);
+//Strictly Equal To
+console.log(num1 === num2);
+// Strictly not equal To
+console.log(num1 !== num2);
