e736b1bdb77221e7f7c088c608d895d1909704b8
Magua William <williammagua@gmail.com>
2024-01-29 19:12:06 +0300

    Finished up with datatypes and did introduction on logical operators and Code logic and control flow

--- script.js
+++ script.js
@@ -58,3 +58,56 @@ console.log(num1 != num2);
 console.log(num1 === num2);
 // Strictly not equal To
 console.log(num1 !== num2);
+
+// Strict vs Loose Equality.
+// Two values are equal if they have the same value
+// They can be of different types (loose equality)
+// Two values are strictly equal if they have the same value and are of the same type
+// If we want them to be of the same type as well then use strict equality operator (===)
+// Loosely Equal
+console.log(5 == "5");
+// Loosely Equal
+console.log(5 == 5);
+// Strictly Equal
+console.log(5 === 5);
+console.log("5" === 5);
+
+console.log("Hello" == "Hello");
+
+// Logical Operators
+// AND Both operand are true &&
+console.log(true && true);
+console.log(true && false);
+// OR Either operand is true ||
+console.log(true || false);
+// NOT Reverses the boolean value !
+console.log(!false);
+
+// DECISION MAKING CODE Logic and control flow
+const age = 18;
+// if(condition){
+//     block of code
+// }
+if(age > 18){
+    console.log("You may enter!") //False coz the age must be greater than 18
+}else if(age === 18){
+    console.log("You're just allowed in, but not for more than 2 Hours!");
+}
+else{
+    console.log("You're NOT allowed in here!");
+}
+
+// while (condition) {
+    // Block of code
+// }
+let i = 0;
+
+while(i < 10) {
+    console.log(i);
+    i++; //iterations
+}
+// for([initialization]; [condition]; [final-expression]) {
+    //Code
+// }
+
+// for(let i = 0; )
