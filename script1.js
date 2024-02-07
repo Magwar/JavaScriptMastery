6427475429d6f1cf0841fa27bdf45fbe6d8bf057
Magua William <williammagua@gmail.com>
2024-02-07 18:41:57 +0300

    Initialised a Js file for the Array methods test

---
+++ script1.js
@@ -0,0 +1,7 @@
+const bikes = ["Honda", "Ducati", "Halley", "Suzuki"];
+console.log(bikes);
+console.log(bikes.toString());
+console.log(bikes.at(1));
+console.log(bikes.join("*"));
+console.log(bikes.pop());
+console.log(bikes.push("Aprillia"));
