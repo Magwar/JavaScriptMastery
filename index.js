5ad998d53a95930ed48f9fc93fe90d6cf7345cd7
Magua William <williammagua@gmail.com>
2024-03-02 19:32:27 +0300

    Learning Js

---
+++ index.js
@@ -0,0 +1,6 @@
+const repeatString = function(string, num) {
+    return string.repeat();
+    };
+    console.log(repeatString("Hello", 3)); // "HelloHelloHello"
+    // Do not edit below this line
+    module.exports = repeatString;
